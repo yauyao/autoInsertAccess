62edb87864a8bae5e4bd76b4c25d3e920cf661b2
Yauyao <eric@df-recycle.com.tw>
2019-02-06 00:56:04 +0800

    Update:新增抓到網頁內容，並新增至Access

--- controller/main.py
+++ controller/main.py
@@ -1,9 +1,28 @@
-from service.access.insertAccess import connectAccess,executeSql,closeAccess
+from service.access.insertAccess import connectAccess,executeSql,closeAccess,searchSql
+from service.webCrawler.crawler import printDatePrice
+import datetime
 
 if __name__ == '__main__':
     con = connectAccess()
-    result = executeSql("select base_info from cis",con)
-    closeAccess(con)
+    result = searchSql("select cis_id,base_info from cis order by cis_id",con)
 
+    content = result[0]
     for content in result:
-        print("url:" + content[0])
+        print("id:%d url:%s" % (content[0],content[1]))
+        list = printDatePrice(content[1])
+
+        search = searchSql("select cis_date from cis_log where cis_id =%d ORDER BY cis_date desc" % content[0],con)
+
+        print("Last Date: %s" % search[0][0])
+
+        for element in list:
+            datetime_obj = datetime.datetime.strptime(element._date, '%Y/%m/%d')
+            if search[0][0] < datetime_obj:
+                insertSql = "insert into cis_log (cis_date,unit_price,cis_id) VALUES ('%s',%f,%s);" % (element._date,float(element._price),content[0])
+                print("     %s" % insertSql)
+                executeSql(insertSql, con)
+            else:
+                print("     have data:%s" % element._date)
+
+
+    closeAccess(con)
--- service/access/insertAccess.py
+++ service/access/insertAccess.py
@@ -2,20 +2,27 @@ import pyodbc
 
 def connectAccess():
     DBfile = "E:\CIS-PC.accdb"  # Access檔案所在地
-    conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ='+DBfile+';')  #連結DateBase
+    conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=%s;' % DBfile )  #連結DateBase
 
-    cursor = conn.cursor()
-    return cursor
+    return conn
 
 def closeAccess(database):
     database.close()
 
-def executeSql(sql,cursor):
+def searchSql(sql,conn):
+    cursor = conn.cursor()
     result = []
     for element in cursor.execute(sql):
         result.append(element)
 
     return result
+
+def executeSql(sql,conn):
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    conn.commit()
+
+
 
 # if __name__ == '__main__':
 #     con = connectAccess()
